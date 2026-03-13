988fd2cde054df2ec0182a87d570b0d96cdce9f2
weterynarzfred <weterynarzfred@gmail.com>
2026-03-13 11:50:58 +0100

    add TODOs

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,6 +1,7 @@
 import Database from "better-sqlite3";
 import path from "path";
 
+// TODO: move the DB into STORAGE_DIR
 const dbPath = path.join(process.cwd(), "shortlib.db");
 const db = new Database(dbPath);
 
